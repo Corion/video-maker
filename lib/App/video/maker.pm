489b6d0d873e11c9d0de17ac0f7d2e85a9a14b6c
Max Maischein <corion@cpan.org>
2019-05-05 19:09:06 +0200

    List prerequisites in Makefile.PL, so we can install the prerequisites automatically

---
+++ lib/App/video/maker.pm
@@ -0,0 +1,16 @@
+package App::video::maker;
+use strict;
+our $VERSION = '0.01';
+
+=head1 NAME
+
+App::video::maker - minimal video processing with ffmpeg and make
+
+=head1 SYNOPSIS
+
+See README.mkdn in the distribution - this module is currently
+a placeholder for the programs distributed with it.
+
+=cut
+
+1;
